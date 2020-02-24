674e9950de09deea21ae423d14904c0abf372304
tibdhond <Tibo.DHondt@UGent.be>
2020-02-24 13:16:27 +0100

    replace if/else chain with function pointers

--- text_to_vc.py
+++ text_to_vc.py
@@ -110,121 +110,143 @@ class Dispatch:
         self.to_shift = {"&": 1, "é": 2, "\"": 3, "'": 4, "(": 5, "§": 6, "è": 7, "!": 8, "ç": 9, "à": 0, "-": "_",
                          ",": "?", ";": ".", ":": "/", "=": "+"}
 
-    def on_press(self, key):
-        if key == Key.enter:
-            if self.enter_pressed:
-                try:
-                    file = ""
-                    if len(self.message) > 0 and self.message[0] == '!':  # Indicates command
-                        c = self.message[1:].lower()
-                        if c == "tts":
-                            self.tts_enabled = not self.tts_enabled
-                        elif c == "lock":
-                            self.app.lock(True)
-                        elif c == "unlock":
-                            self.app.lock(False)
-                        elif c == "stop":
-                            sd.stop()
-                        elif c == "quit" or c == "exit":
-                            self.app.quit()
-                            sys.exit()
-                        else:
-                            self.app.color(c)
-                    elif self.message[-4:] == ".mp3":
-                        file = "Soundboard/{0}".format(self.message)
-                    else:
-                        if self.tts_enabled:
-                            file = "message.mp3"
-                            self.message = re.sub("([a-zA-Z]{2,3})[cC][cC] ", r'\1ck', self.message)
-                            self.message = re.sub("([a-zA-Z]{2,3})[cC][cC]$", r'\1ck', self.message)
-                            self.message.replace("wtf", "what the fuck")
-                            t2s = gTTS(text=self.message, lang='en-US')
-                            t2s.save("message.mp3")
-                    if file != "":
-                        sr, array = read(file, True)
-                        sd.default.device = 4
-                        sd.play(array, sr)
-                except AssertionError:
-                    print()
-                except FileNotFoundError:
-                    print()
+        self.top_dispatch = {Key.enter: self.on_enter, Key.esc: self.on_esc}
+        self.bottom_dispatch = {Key.backspace: self.on_backspace, Key.delete: self.on_delete,
+                                Key.shift: self.on_shift, Key.tab: self.on_tab, Key.left: self.on_left,
+                                Key.right: self.on_right, Key.up: self.on_up, Key.down: self.on_down,
+                                Key.space: self.on_space}
 
-                self.index = 0
-                self.matchIndex = -1
-                if self.history_index != len(self.history):
-                    self.history.pop()
-                if self.message != "" and (len(self.history) == 0 or self.history[-1] != self.message):
-                    self.history.append(self.message)
-                if len(self.history) > 20:  # History size
-                    self.history.pop(0)
-                self.history_index = len(self.history)
-                self.message = ""
-                self.app.not_recording()
-                self.enter_pressed = False
-            else:
-                self.app.recording()
-                self.enter_pressed = True
-        elif key == Key.esc:
-            self.app.not_recording()
-            self.enter_pressed = False
-        elif self.enter_pressed:
-            if key == Key.backspace:
-                self.matchIndex = -1
-                if self.index > 0:
-                    self.message = self.message[:self.index - 1] + self.message[self.index:]
-                    self.index -= 1
-            elif key == Key.delete:
-                self.matchIndex = -1
-                if self.index < len(self.message):
-                    self.message = self.message[:self.index] + self.message[self.index + 1:]
-            elif key == Key.shift:
-                self.shift_pressed = True
-            elif key == Key.tab:
-                if self.matchIndex == -1:
-                    self.matches = list(filter(lambda x: re.match(re.escape(self.message[:self.index]),
-                                                                  x, re.IGNORECASE), self.options))
-                if len(self.matches) > 0:
-                    self.matchIndex = (self.matchIndex + 1) % len(self.matches)
-                    self.message = self.matches[self.matchIndex]
-                    self.index = len(self.message)
-            elif key == Key.left:
-                self.index = max(self.index - 1, 0)
-            elif key == Key.right:
-                self.index = min(self.index + 1, len(self.message))
-            elif key == Key.up and self.history_index > 0:
-                if self.message != "":
-                    self.history.insert(self.history_index, self.message)
-                self.history_index = max(0, self.history_index - 1)
-                self.message = self.history.pop(self.history_index)
-            elif key == Key.down:
-                if self.message != "":
-                    self.history.insert(self.history_index, self.message)
-                self.history_index = min(self.history_index + 1, len(self.history))
-                if self.history_index < len(self.history):
-                    self.message = self.history.pop(self.history_index)
-                else:
-                    self.message = ""
-            else:
-                if key == Key.space:
-                    self.matchIndex = -1
-                    self.message += ' '
-                    self.index += 1
-                else:
-                    self.matchIndex = -1
-                    key = str(key)
-                    if key.count('"') > 1:
-                        key = key.replace('"', '')
-                    elif key.count("'") > 1:
-                        key = key.replace("'", "")
-                    if len(key) == 1:
-                        if self.shift_pressed:
-                            key = str(self.to_shift.get(key, key.upper()))
-                            self.shift_pressed = False
-                        self.message = self.message[:self.index] + key + self.message[self.index:]
-                        self.index = self.index + 1
+    def on_press(self, key):
+        self.top_dispatch.get(key, self.on_any)(key)
 
         self.app.message(self.message)
         self.app.cursor(self.index)
+
+    def on_enter(self, key):
+        if self.enter_pressed:
+            try:
+                file = ""
+                if len(self.message) > 0 and self.message[0] == '!':  # Indicates command
+                    c = self.message[1:].lower()
+                    if c == "tts":
+                        self.tts_enabled = not self.tts_enabled
+                    elif c == "lock":
+                        self.app.lock(True)
+                    elif c == "unlock":
+                        self.app.lock(False)
+                    elif c == "stop":
+                        sd.stop()
+                    elif c == "quit" or c == "exit":
+                        self.app.quit()
+                        sys.exit()
+                    else:
+                        self.app.color(c)
+                elif self.message[-4:] == ".mp3":
+                    file = "Soundboard/{0}".format(self.message)
+                else:
+                    if self.tts_enabled:
+                        file = "message.mp3"
+                        self.message = re.sub("([a-zA-Z]{2,3})[cC][cC] ", r'\1ck', self.message)
+                        self.message = re.sub("([a-zA-Z]{2,3})[cC][cC]$", r'\1ck', self.message)
+                        self.message.replace("wtf", "what the fuck")
+                        t2s = gTTS(text=self.message, lang='en-US')
+                        t2s.save("message.mp3")
+                if file != "":
+                    sr, array = read(file, True)
+                    sd.default.device = 4
+                    sd.play(array, sr)
+            except AssertionError:
+                print()
+            except FileNotFoundError:
+                print()
+
+            self.index = 0
+            self.matchIndex = -1
+            if self.history_index != len(self.history):
+                self.history.pop()
+            if self.message != "" and (len(self.history) == 0 or self.history[-1] != self.message):
+                self.history.append(self.message)
+            if len(self.history) > 20:  # History size
+                self.history.pop(0)
+            self.history_index = len(self.history)
+            self.message = ""
+            self.app.not_recording()
+            self.enter_pressed = False
+        else:
+            self.app.recording()
+            self.enter_pressed = True
+
+    def on_esc(self, key):
+        self.app.not_recording()
+        self.enter_pressed = False
+
+    def on_backspace(self, key):
+        self.matchIndex = -1
+        if self.index > 0:
+            self.message = self.message[:self.index - 1] + self.message[self.index:]
+            self.index -= 1
+
+    def on_delete(self, key):
+        self.matchIndex = -1
+        if self.index < len(self.message):
+            self.message = self.message[:self.index] + self.message[self.index + 1:]
+
+    def on_shift(self, key):
+        self.shift_pressed = True
+
+    def on_tab(self, key):
+        if self.matchIndex == -1:
+            self.matches = list(filter(lambda x: re.match(re.escape(self.message[:self.index]),
+                                                          x, re.IGNORECASE), self.options))
+        if len(self.matches) > 0:
+            self.matchIndex = (self.matchIndex + 1) % len(self.matches)
+            self.message = self.matches[self.matchIndex]
+            self.index = len(self.message)
+
+    def on_left(self, key):
+        self.index = max(self.index - 1, 0)
+
+    def on_right(self, key):
+        self.index = min(self.index + 1, len(self.message))
+
+    def on_up(self, key):
+        if self.history_index > 0:
+            if self.message != "":
+                self.history.insert(self.history_index, self.message)
+            self.history_index = max(0, self.history_index - 1)
+            self.message = self.history.pop(self.history_index)
+
+    def on_down(self, key):
+        if self.message != "":
+            self.history.insert(self.history_index, self.message)
+        self.history_index = min(self.history_index + 1, len(self.history))
+        if self.history_index < len(self.history):
+            self.message = self.history.pop(self.history_index)
+        else:
+            self.message = ""
+
+    def on_space(self, key):
+        self.matchIndex = -1
+        self.message += ' '
+        self.index += 1
+
+    def on_non_special(self, key):
+        self.matchIndex = -1
+        key = str(key)
+        if key.count('"') > 1:
+            key = key.replace('"', '')
+        elif key.count("'") > 1:
+            key = key.replace("'", "")
+        if len(key) == 1:
+            if self.shift_pressed:
+                key = str(self.to_shift.get(key, key.upper()))
+                self.shift_pressed = False
+            self.message = self.message[:self.index] + key + self.message[self.index:]
+            self.index = self.index + 1
+
+    def on_any(self, key):
+        if self.enter_pressed:
+            self.bottom_dispatch.get(key, self.on_non_special)(key)
 
 
 def loop(dispatch):
